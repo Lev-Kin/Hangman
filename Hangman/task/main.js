a8375db36b8f115f545e7f66f9072e674fd85f35
Leva <parimalleva@gmail.com>
2022-09-25 13:42:51 +0300

    2 of 8 stages completed

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -1,2 +1,11 @@
-console.log("H A N G M A N");
-console.log("The game will be available soon.");
+const input = require('sync-input');
+
+console.log('H A N G M A N')
+console.log('Guess the word: ')
+
+const word = input();
+if (word === 'python') {
+    console.log('You survived!')
+} else {
+    console.log('You lost!')
+}
