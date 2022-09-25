55e8772ab14e15ca2105a34db5400005fc00254e
Leva <parimalleva@gmail.com>
2022-09-25 15:08:46 +0300

    3 of 8 stages completed

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -3,8 +3,11 @@ const input = require('sync-input');
 console.log('H A N G M A N')
 console.log('Guess the word: ')
 
-const word = input();
-if (word === 'python') {
+const words = ['python', 'java', 'swift', 'javascript'];
+const index = Math.floor(Math.random() * words.length);
+
+const userWord = input();
+if (words[index] === userWord) {
     console.log('You survived!')
 } else {
     console.log('You lost!')
